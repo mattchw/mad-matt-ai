def0f00731f4fc0c4f7f5e34be5092ea66994c59
Matthew Wong <matthew.chohin@gmail.com>
2023-05-01 11:13:29 +0100

    fix(chat): loading and recording styles

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -3,11 +3,12 @@ import Image from 'next/image';
 
 type Props = {
   chat: ChatInterface;
+  isLoading?: boolean;
 }
 
 const aiImage = '/images/mad-matt.png';
 
-function Chat({ chat }: Props) {
+function Chat({ chat, isLoading }: Props) {
   return (
     <div
       className={`flex ${chat.type === 'question'
@@ -17,16 +18,29 @@ function Chat({ chat }: Props) {
     >
       {chat.type === 'answer' && (
         <Image
-          className="w-16 h-16 rounded-full mr-3"
+          className="w-16 h-16 rounded-full mr-5"
           src={aiImage}
           width={120}
           height={120}
           alt="Mad Matt"
         />
       )}
-      <h6 className={`w-full leading-normal text-slate-200 sm:leading-7 ${chat.type === 'question' ? 'text-end' : ''}`}>
-        {chat.text}
-      </h6>
+      {
+        isLoading ? (
+          <div className="w-full">
+            <>
+              <div className="animate-pulse">
+                <div className="h-4 bg-gray-300 rounded"></div>
+                <div className="h-4 bg-gray-300 rounded mt-2"></div>
+                <div className="h-4 bg-gray-300 rounded mt-2"></div>
+                <div className="h-4 bg-gray-300 rounded mt-2"></div>
+              </div>
+            </>
+          </div>
+        ) : <h6 className={`w-full leading-normal text-slate-200 sm:leading-7 ${chat.type === 'question' ? 'text-end' : ''}`}>
+          {chat.text}
+        </h6>
+      }
     </div>
   )
 }
